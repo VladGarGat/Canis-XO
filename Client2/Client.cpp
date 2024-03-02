c131f7a4d5a92a636e883d1e1caa47eb8b21cf76
Vlad <96255646+VladGarGat@users.noreply.github.com>
2024-03-03 01:13:20 +0300

    Update Client.cpp

--- Client2/Client.cpp
+++ Client2/Client.cpp
@@ -1,6 +1,6 @@
-﻿#pragma comment(lib, "ws2_32.lib")
-#define __STDC_WANT_LIB_EXT1__ 1
-#include <winsock2.h>
+#pragma comment(lib, "ws2_32.lib")
+#include <WinSock2.h>
+
 #include <iostream>
 #include <stdio.h>
 #include <string>
@@ -10,6 +10,10 @@
 
 char board[20] = {};
 
+char a_login[256];
+char a_passw[256];
+bool a_flag = false;
+
 void print_board(char*) {
     printf("Game board\n");
     printf(" %c | %c | %c \n", board[0], board[1], board[2]);
@@ -18,7 +22,6 @@ void print_board(char*) {
     printf("---+---+---\n");
     printf(" %c | %c | %c \n", board[6], board[7], board[8]);
 }
-
 
 int get_move(void) {
     printf("\nMenu of the moves:\n");
@@ -39,7 +42,6 @@ int get_move(void) {
     return move;
 }
 
-
 bool has_wonQ(char player) {
     int wins[][3] = { {0,1,2},{3,4,5},{6,7,8},{0,3,6},{1,4,7},{2,5,8},{0,4,8},{2,4,6} };
     for (int i = 0; i < 8; i++) {
@@ -54,9 +56,59 @@ bool has_wonQ(char player) {
     return false;
 }
 
+bool FoundUserData(std::ifstream& f1)
+{
+    f1.clear(); f1.seekg(0);
+    std::string flag;
+    while (f1.peek() != EOF) {
+        std::getline(f1, flag);
+        if (flag == "%AUTO_LOGGING_IN%") {
+            std::getline(f1, flag);
+            strcpy(a_login, flag.c_str());
+            std::getline(f1, flag);
+            strcpy(a_passw, flag.c_str());
+
+            return true;
+        }
+    }
+    return false;
+}
+
 int main(int argc, char* argv[]) {
+
+    // В командую строку передается любое кол-во аргументов.
+    // Название файла конфигурации может быть подано в виде:
+    // -cfg conf.txt
+    // обязательно: -cfg   название любое: conf.txt
+
+    std::ifstream f1;
+    std::string cfgname = "config.txt";
+    if (argc > 2) {
+        std::string str;
+        for (int i = 1; i < argc - 1; i += 2) {
+            str = argv[i];
+            if (str == "-cfg" && ((i + 1) < argc)) {
+                cfgname = argv[i + 1];
+                break;
+            }
+        }
+    }
+    f1.open(cfgname);
+
+    // читаем название лог-файла, которое должно быть первой строкой в конфиге
     std::ofstream f;
-    f.open("log.txt");
+    std::string logname;
+    std::getline(f1, logname);
+    f.open(logname);
+
+    // читаем две строки: адрес и порт соответственно следующими строчками из конфига
+    std::string s_ip; std::string s_port;
+    std::getline(f1, s_ip);
+    std::getline(f1, s_port);
+    const char* ip = s_ip.c_str();
+    int port = stoi(s_port);
+
+
     WSAData wsaData;
     WORD DLLVersion = MAKEWORD(2, 1);
     if (WSAStartup(DLLVersion, &wsaData) != 0) {
@@ -67,8 +119,8 @@ int main(int argc, char* argv[]) {
     char* dt = ctime(&now);
     f << ((std::string)dt + " - Sockets start up!") << std::endl << std::endl;
     SOCKADDR_IN addr;
-    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
-    addr.sin_port = htons(1111);
+    addr.sin_addr.s_addr = inet_addr(ip);
+    addr.sin_port = htons(port);
     addr.sin_family = AF_INET;
     SOCKET Connection = socket(AF_INET, SOCK_STREAM, NULL);
     now = time(0); dt = ctime(&now);
@@ -85,24 +137,30 @@ int main(int argc, char* argv[]) {
         std::cout << "Connect with server!";
         now = time(0); dt = ctime(&now);
         f << ((std::string)dt + " - Server was conected. Socket: ") << Connection << std::endl << std::endl;
-        std::cout << "Username: ";
-        char username[256];
-        std::cin >> username;
-        std::cout << "Password: ";
-        char password[256];
-        std::cin >> password;
+
+        // если FoundUserData, то мы введем данные автоматически, но только единожды
         char confirm[256];
-        send(Connection, username, sizeof(username), NULL);
-        now = time(0); dt = ctime(&now);
-        f << ((std::string)dt + " - Send to server username: ") << username << std::endl << std::endl;
 
-        send(Connection, password, sizeof(password), NULL);
-        now = time(0); dt = ctime(&now);
-        f << ((std::string)dt + " - Send to server password: ") << password << std::endl << std::endl;
+        if (FoundUserData(f1)) {
+            std::cout << "Trying automatic authorization...\n";
+            send(Connection, a_login, sizeof(a_login), NULL);
+            now = time(0); dt = ctime(&now);
+            f << ((std::string)dt + " - Send to server username: ") << a_login << std::endl << std::endl;
 
-        recv(Connection, confirm, sizeof(confirm), NULL);
-        now = time(0); dt = ctime(&now);
-        f << ((std::string)dt + " - Authorisation ") << confirm << std::endl << std::endl;
+            send(Connection, a_passw, sizeof(a_passw), NULL);
+            now = time(0); dt = ctime(&now);
+            f << ((std::string)dt + " - Send to server password: ") << a_passw << std::endl << std::endl;
+
+            recv(Connection, confirm, sizeof(confirm), NULL);
+            now = time(0); dt = ctime(&now);
+            f << ((std::string)dt + " - Authorisation ") << confirm << std::endl << std::endl;
+
+            if (strcmp(confirm, "Accept") != 0) { std::cout << "wrong preloaded user data from config. Switching to manual autorization.\n"; }
+        }
+
+        char username[256];
+        char password[256];
+
         while (strcmp(confirm, "Accept") != 0) {
             std::cout << "Username: ";
             std::cin >> username;
